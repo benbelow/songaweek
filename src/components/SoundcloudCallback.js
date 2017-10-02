b6a26caf1f8fae75aa8b4b8ab45c1dd37a10025c
Benjamin Below <Benjamin.Below@softwire.com>
2017-10-02 21:27:37 +0100

    Make callback component a class

--- src/components/SoundcloudCallback.js
+++ src/components/SoundcloudCallback.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export default () => {
-  return (<div>
-      <h1>Connect with SoundCloud</h1>
-      <body onload="window.setTimeout(window.opener.SC.connectCallback, 1);">
-      <p>
-        This popup should automatically close in a few seconds
-      </p>
-      </body>
-    </div>
-  );
+class Callback extends React.Component {
 
+  componentDidMount() {
+    window.setTimeout(window.opener.SC.connectCallback, 1);
+  }
+
+  render() {
+    return <div><p>This page should close soon.</p></div>;
+  }
 }
+
+export default Callback;
