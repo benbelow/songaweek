f5bb06df9e375af0dccde8bfa60aa1c7a1b4d851
Ben Below <benjamin.below@softwire.com>
2018-01-22 19:30:01 +0000

    Add refresh button that pulls all threads

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,41 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Flexbox from 'flexbox-react';
 import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+
+import {syncData, toggleMenu} from "./HeaderActions";
+import AutoRenew from 'material-ui/svg-icons/action/autorenew';
 import snoo from '../../snoo.png';
 import '../App/App.css';
-import { connect } from "react-redux";
-import { toggleMenu } from "./HeaderActions";
 
 class AppHeader extends Component {
-  static propTypes = {
-    toggleMenu: PropTypes.func,
-  };
+    static propTypes = {
+        toggleMenu: PropTypes.func,
+        syncData: PropTypes.func,
+    };
 
-  appVersion = '0.6.1';
+    appVersion = '0.6.1';
 
-  render() {
-    return (
-      <div className="App-header">
-        <Flexbox flexDirection='row' justifyContent='space-between'>
-          <Flexbox flexDirection='row'>
-            <img src={snoo} className="App-logo" alt="logo"/>
-            <h2 onClick={() => window.open('https://www.reddit.com/r/songaweek/')} className='App-title'>
-              The Song A Week Challenge
-            </h2>
-          </Flexbox>
-          <p style={{ fontSize: 8 }}>v{this.appVersion}</p>
-        </Flexbox>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App-header">
+                <Flexbox flexDirection='row' justifyContent='space-between'>
+                    <Flexbox flexDirection='row'>
+                        <img src={snoo} className="App-logo" alt="logo"/>
+                        <h2 onClick={() => window.open('https://www.reddit.com/r/songaweek/')} className='App-title'>
+                            The Song A Week Challenge
+                        </h2>
+                        <AutoRenew
+                            style={{ marginLeft: 12, paddingTop: '5%', width: 40, height: 40, cursor: 'pointer' }}
+                            color={'#FFFFFF'}
+                            onClick={this.props.syncData}
+                        />
+                    </Flexbox>
+                    <p style={{fontSize: 8}}>v{this.appVersion}</p>
+                </Flexbox>
+            </div>
+        );
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    toggleMenu: () => {
-      dispatch(toggleMenu());
+    return {
+        toggleMenu: () => dispatch(toggleMenu()),
+        syncData: () => dispatch(syncData()),
     }
-  }
 };
 
 export default connect(null, mapDispatchToProps)(AppHeader);
--- src/components/AppHeader/HeaderActions.js
+++ src/components/AppHeader/HeaderActions.js
@@ -1,7 +1,25 @@
+import {database} from "../../database";
+import {fetchAllThreads} from "../ThreadFetcher/ThreadFetcherActions";
+
 export const TOGGLE_MENU = 'TOGGLE_MENU';
+export const SYNC_DATA = 'SYNC_DATA';
 
 export function toggleMenu() {
-  return {
-    type: TOGGLE_MENU,
-  }
+    return {
+        type: TOGGLE_MENU,
+    }
+}
+
+export function syncData() {
+    return async dispatch => {
+        console.log(database);
+
+        await dispatch(fetchAllThreads());
+
+        console.log('finished fetching all');
+
+        return {
+            type: SYNC_DATA,
+        }
+    }
 }
