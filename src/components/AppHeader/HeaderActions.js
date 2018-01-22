2a8909e21ea3d647f37c914b6ae7bff163381d10
Ben Below <benjamin.below@softwire.com>
2018-01-22 20:11:26 +0000

    Make sync button sync all submission data as well as thread data

--- src/components/AppHeader/HeaderActions.js
+++ src/components/AppHeader/HeaderActions.js
@@ -1,5 +1,9 @@
+import _ from 'lodash';
+
 import {database} from "../../database";
 import {fetchAllThreads} from "../ThreadFetcher/ThreadFetcherActions";
+import {fetchSubmissions} from "../SubmissionThread/SubmissionThreadActions";
+import Formatter from "../Submission/Formatter";
 
 export const TOGGLE_MENU = 'TOGGLE_MENU';
 export const SYNC_DATA = 'SYNC_DATA';
@@ -12,14 +16,24 @@ export function toggleMenu() {
 
 export function syncData() {
     return async dispatch => {
-        console.log(database);
+        const allThreads = await dispatch(fetchAllThreads());
 
-        await dispatch(fetchAllThreads());
-
-        console.log('finished fetching all');
+        _.forEach(allThreads, async t => {
+            database.ref(`threads/${t.id}`).set(t);
+            await dispatch(fetchSubmissions(t.id, t.url))
+                .then(ts => _.each(ts, s => {
+                    const formatter = new Formatter(s.comment);
+                    const genre = formatter.genre() || null;
+                    const link = formatter.markdownLink() || null;
+                    const description = formatter.description() || null;
+                    const themed = formatter.themed() || null;
+                    database.ref(`submissions/${s.commentId}`).set(_.merge({}, s, { genre, link, description, themed }));
+                }))
+                .then(() => console.log(`Finished syncing thread id: ${t.id}`));
+        });
 
         return {
             type: SYNC_DATA,
-        }
+        };
     }
 }
--- src/components/SubmissionThread/SubmissionThreadActions.js
+++ src/components/SubmissionThread/SubmissionThreadActions.js
@@ -7,34 +7,40 @@ export const UPDATE_SUBMISSIONS = 'UPDATE_SUBMISSIONS';
 const REDDIT_KIND_ID_COMMENT = 't1';
 
 const containsValidSubmissionUrl = comment => {
-  return _.filter(extractUrls(comment), u => isValidSubmissionUrl(u)).length > 0;
+    return _.filter(extractUrls(comment), u => isValidSubmissionUrl(u)).length > 0;
 };
 
 export function updateSubmissions(threadSubmissions) {
-  return {
-    type: UPDATE_SUBMISSIONS,
-    threadSubmissions,
-  }
+    return {
+        type: UPDATE_SUBMISSIONS,
+        threadSubmissions,
+    }
 }
 
 export function fetchSubmissions(threadId, url) {
-  return dispatch => {
-    fetch(url + ".json?limit=1000", { method: 'get', mode: 'cors' })
-      .then(function(response) {
-        return response.json();
-      })
-      .then(data => {
-          return data[1].data.children.filter(c => c.kind === REDDIT_KIND_ID_COMMENT).map(i => {
-          return {
-            comment: i.data.body,
-            author: i.data.author,
-            threadId: i.data.parent_id,
-            commentId: i.data.name,
-            subredditNamePrefixed: i.data.subreddit_name_prefixed,
-          }
-        });
-      })
-      .then(results => _.filter(results, r => containsValidSubmissionUrl(r.comment)))
-      .then(submissions => dispatch(updateSubmissions({threadId: threadId, submissions: submissions})))
-  }
+    return async dispatch => {
+        let submissionsForThread;
+        await fetch(url + ".json?limit=1000", {method: 'get', mode: 'cors'})
+            .then(function (response) {
+                return response.json();
+            })
+            .then(data => {
+                return data[1].data.children.filter(c => c.kind === REDDIT_KIND_ID_COMMENT).map(i => {
+                    return {
+                        comment: i.data.body,
+                        author: i.data.author,
+                        threadId: i.data.parent_id,
+                        commentId: i.data.name,
+                        subredditNamePrefixed: i.data.subreddit_name_prefixed,
+                    }
+                });
+            })
+            .then(results => _.filter(results, r => containsValidSubmissionUrl(r.comment)))
+            .then(submissions => {
+                submissionsForThread = submissions;
+                dispatch(updateSubmissions({threadId: threadId, submissions: submissions}))
+            });
+
+        return submissionsForThread;
+    }
 }
