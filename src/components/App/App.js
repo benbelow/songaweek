271b8f8e2efd7997b4fbf50b8cd7fc9e4aed9df8
Ben Below <benjamin.below@softwire.com>
2018-03-15 09:26:29 +0000

    Move all routing to app component
    Fixes issue with nested redirect, causing Soundcloud login callback to behave incorrectly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,44 @@
-import React, {Component} from 'react';
-import {Provider} from 'react-redux';
-import {MuiThemeProvider} from 'material-ui';
-import './App.css';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { MuiThemeProvider } from 'material-ui';
+import Route from "react-router/es/Route";
+import Redirect from "react-router/es/Redirect";
+import { BrowserRouter } from "react-router-dom";
+import Switch from "react-router/es/Switch";
 import store from '../../redux/store';
 import ThreadFetcher from "../ThreadFetcher/ThreadFetcher";
 import SubmissionThreads from "../SubmissionThreads";
 import AppHeader from "../AppHeader/AppHeader";
 import Filters from "../Filters/Filters";
-import Route from "react-router/es/Route";
-import Redirect from "react-router/es/Redirect";
 import Users from "../Users/Users";
-import Switch from "react-router/es/Switch";
 import Admin from "../Admin/Admin";
+import SoundcloudCallback from "../SoundcloudCallback";
+
+import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <Provider store={store()}>
-                <MuiThemeProvider>
-                    <div className="App">
-                        <AppHeader/>
-                        <Filters/>
-                        <div className="App-body">
-                            <ThreadFetcher/>
-                            <Switch>
-                                <Route path='/threads' component={SubmissionThreads}/>
-                                <Route path='/users' component={Users}/>
-                                <Route path='/admin' component={Admin}/>
-                                <Redirect from='*' to='threads'/>
-                            </Switch>
+            <BrowserRouter>
+                <Provider store={store()}>
+                    <MuiThemeProvider>
+                        <div className="App">
+                            <AppHeader/>
+                            <Filters/>
+                            <div className="App-body">
+                                <ThreadFetcher/>
+                                <Switch>
+                                    <Route path='/threads' component={SubmissionThreads}/>
+                                    <Route path='/users' component={Users}/>
+                                    <Route path='/admin' component={Admin}/>
+                                    <Route path="/callback.html" component={SoundcloudCallback}/>
+                                    <Redirect from='*' to='threads'/>
+                                </Switch>
+                            </div>
                         </div>
-                    </div>
-                </MuiThemeProvider>
-            </Provider>
+                    </MuiThemeProvider>
+                </Provider>
+            </BrowserRouter>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Route from 'react-router/es/Route';
-import {BrowserRouter} from 'react-router-dom'
 
 import App from './components/App/App';
-import SoundcloudCallback from "./components/SoundcloudCallback";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <Route path="/" component={App}/>
-            <Route path="/callback.html" component={SoundcloudCallback}/>
-        </div>
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
