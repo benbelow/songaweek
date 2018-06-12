e75a7086aad4d44cae739bb6e9b0c7ae3c349e69
Ben Below <benjamin.below@softwire.com>
2018-06-12 13:59:22 +0100

    Add tests for fetchAllThreads

--- src/scenes/submissionThreads/components/ThreadFetcher/ThreadFetcherActions.test.js
+++ src/scenes/submissionThreads/components/ThreadFetcher/ThreadFetcherActions.test.js
@@ -1,26 +1,44 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import fetchMock from 'fetch-mock';
-import { FETCH_THREADS, fetchThreads } from './ThreadFetcherActions';
+import { FETCH_THREADS, fetchAllThreads, fetchThreads } from './ThreadFetcherActions';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 const submissionThreadTitle = 'Submissions - Week 12';
 const nonSubmissionThreadTitle = 'Not a submissions thread.';
-const getRedditApiResponse = threadTitles => {
-    return JSON.stringify({
+
+class RedditApiResponseBuilder {
+    response = {
         data: {
-            children: threadTitles.map(tt => ({ data: { title: tt } }))
+            children: [],
+            after: null,
         }
-    });
-};
+    };
+
+    withThreads(threadTitles) {
+        this.response.data.children = threadTitles.map(tt => ({ data: { title: tt } }));
+        return this;
+    }
+
+    withAfterFlag(after) {
+        this.response.data.after = after;
+        return this;
+    }
+
+    build() {
+        return JSON.stringify(this.response);
+    }
+}
 
 describe('thread fetcher actions', () => {
     let store;
+    let responseBuilder;
 
     beforeEach(() => {
         store = mockStore({});
+        responseBuilder = new RedditApiResponseBuilder();
     });
 
     afterEach(() => {
@@ -28,9 +46,9 @@ describe('thread fetcher actions', () => {
         fetchMock.restore();
     });
 
-    describe('fetch threads', () => {
-        it('creates FETCH_THREADS action', async () => {
-            fetchMock.getOnce('*', getRedditApiResponse([submissionThreadTitle]));
+    describe('fetchThreads', () => {
+        it('dispatches FETCH_THREADS action', async () => {
+            fetchMock.getOnce('*', responseBuilder.withThreads([submissionThreadTitle]).build());
 
             await store.dispatch(fetchThreads());
 
@@ -38,7 +56,7 @@ describe('thread fetcher actions', () => {
         });
 
         it('only fetches submission threads', async () => {
-            fetchMock.getOnce('*', getRedditApiResponse([submissionThreadTitle, nonSubmissionThreadTitle]));
+            fetchMock.getOnce('*', responseBuilder.withThreads([submissionThreadTitle, nonSubmissionThreadTitle]).build());
 
             await store.dispatch(fetchThreads());
 
@@ -46,12 +64,77 @@ describe('thread fetcher actions', () => {
         });
 
         it('only fetches specified number of results', async () => {
-            fetchMock.getOnce('*', getRedditApiResponse([submissionThreadTitle, nonSubmissionThreadTitle]));
+            fetchMock.getOnce('*', responseBuilder.build());
             const resultLimit = 32;
 
             await store.dispatch(fetchThreads(resultLimit));
 
-            expect(fetchMock.lastUrl()).toContain(`&limit=${resultLimit}`)
+            expect(fetchMock.lastUrl()).toContain(`&limit=${resultLimit}`);
+        });
+    });
+
+    describe('fetchAllThreads', () => {
+        it('dispatches FETCH_THREADS action', async () => {
+            fetchMock.getOnce('*', responseBuilder.withThreads([submissionThreadTitle]).build());
+
+            await store.dispatch(fetchAllThreads());
+
+            expect(store.getActions().map(a => a.type)).toContain(FETCH_THREADS);
+        });
+
+        it('fetches multiple pages of results', async () => {
+            const page1AfterFlag = 'page-1';
+            const page2AfterFlag = 'page-2';
+
+            fetchMock.getOnce(
+                `end:&after=${page2AfterFlag}`,
+                responseBuilder.withThreads([submissionThreadTitle]).build()
+            );
+            fetchMock.getOnce(
+                `end:&after=${page1AfterFlag}`,
+                responseBuilder.withThreads([submissionThreadTitle]).withAfterFlag(page2AfterFlag).build()
+            );
+            fetchMock.getOnce(
+                'end:after=',
+                responseBuilder.withThreads([submissionThreadTitle]).withAfterFlag(page1AfterFlag).build()
+            );
+
+            await store.dispatch(fetchAllThreads());
+
+            expect(fetchMock.calls().length).toBe(3);
+        });
+
+        it('stops fetching results when no results found', async () => {
+            fetchMock.getOnce('*', responseBuilder.withAfterFlag('something').build());
+
+            await store.dispatch(fetchAllThreads());
+
+            expect(fetchMock.calls().length).toBe(1);
+        });
+
+        it('stops fetching results when no after flag set in response', async () => {
+            fetchMock.getOnce('*', responseBuilder.withThreads([submissionThreadTitle]).build());
+
+            await store.dispatch(fetchAllThreads());
+
+            expect(fetchMock.calls().length).toBe(1);
+        });
+
+        it('returns threads from all pages of responses', async () => {
+            const afterFlag = 'page-1';
+
+            fetchMock.getOnce(
+                `end:&after=${afterFlag}`,
+                responseBuilder.withThreads([submissionThreadTitle]).build()
+            );
+            fetchMock.getOnce(
+                'end:after=',
+                responseBuilder.withThreads([submissionThreadTitle]).withAfterFlag(afterFlag).build()
+            );
+
+            const results = await store.dispatch(fetchAllThreads());
+
+            expect(results.length).toBe(2);
         });
     });
 });
