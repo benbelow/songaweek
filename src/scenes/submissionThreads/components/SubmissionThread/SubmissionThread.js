4c57bea81e7b7627853c4e4c16d8697a2d7461c8
Ben Below <benjamin.below@softwire.com>
2018-06-19 13:38:23 +0100

    Some tests for SubmissionThread component

--- src/scenes/submissionThreads/components/SubmissionThread/SubmissionThread.js
+++ src/scenes/submissionThreads/components/SubmissionThread/SubmissionThread.js
@@ -9,186 +9,185 @@ import _ from "lodash";
 import { fetchSubmissions } from "./redux/SubmissionThreadActions";
 import Submission from "../Submission/components/Submission";
 import ParsedSubmission from "../../../../models/submission/parsedSubmission";
-import { extractUrls } from "../../../../services/UrlParsingService/UrlParsingService";
-import { isSoundCloudUrl } from "../../../../services/UrlParsingService/UrlParsingService"
+import { extractUrls, isSoundCloudUrl } from "../../../../services/UrlParsingService/UrlParsingService";
 import { generatePlaylist, getPlaylistLinkForThread } from "../../../../integrations/soundcloud/PlaylistGenerator";
 
 
-class SubmissionThread extends Component {
-  static propTypes = {
-    thread: PropTypes.object.isRequired,
-    threadSubmissions: PropTypes.array,
-    isThemedFilter: PropTypes.bool,
-    isPrivateFilter: PropTypes.bool,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      playlistTitle: undefined,
-      playlistLink: undefined,
-      shouldShowPlaylist: false,
-      loadingPlaylist: false,
+export class SubmissionThread extends Component {
+    static propTypes = {
+        thread: PropTypes.object.isRequired,
+        threadSubmissions: PropTypes.array,
+        isThemedFilter: PropTypes.bool,
+        isPrivateFilter: PropTypes.bool,
     };
-    this.submissions = this.submissions.bind(this);
-    this.fetchSubmissions = this.fetchSubmissions.bind(this);
-    this.shouldShowGenerateScPlaylistButton = this.shouldShowGenerateScPlaylistButton.bind(this);
-    this.generateSoundcloudPlaylist = this.generateSoundcloudPlaylist.bind(this);
-  }
 
-  cardStyle = {
-    margin: 16,
-  };
-
-  flexboxStyle = {
-    display: 'flex',
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
-    alignContent: 'stretch'
-  };
-
-  cachedSubmissions;
-
-  themedFilter = submission => {
-    if (!this.props.isThemedFilter) {
-      return () => true;
+    constructor(props) {
+        super(props);
+        this.state = {
+            playlistTitle: undefined,
+            playlistLink: undefined,
+            shouldShowPlaylist: false,
+            loadingPlaylist: false,
+        };
+        this.submissions = this.submissions.bind(this);
+        this.fetchSubmissions = this.fetchSubmissions.bind(this);
+        this.shouldShowGenerateScPlaylistButton = this.shouldShowGenerateScPlaylistButton.bind(this);
+        this.generateSoundcloudPlaylist = this.generateSoundcloudPlaylist.bind(this);
     }
-    const parsedSubmission = new ParsedSubmission(submission.comment);
-    return parsedSubmission.themed();
-  };
 
-  submissions() {
-    if (!this.cachedSubmissions) {
-      let currentThread = _.first(_.filter(this.props.threadSubmissions, ts => this.props.thread.id === ts.threadId));
+    cardStyle = {
+        margin: 16,
+    };
 
-      if (!currentThread) {
-        this.cachedSubmissions = null;
-      } else {
-        this.cachedSubmissions = currentThread.submissions;
-      }
+    flexboxStyle = {
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'space-around',
+        alignContent: 'stretch'
+    };
+
+    cachedSubmissions;
+
+    themedFilter = submission => {
+        if (!this.props.isThemedFilter) {
+            return () => true;
+        }
+        const parsedSubmission = new ParsedSubmission(submission.comment);
+        return parsedSubmission.themed();
+    };
+
+    submissions() {
+        if (!this.cachedSubmissions) {
+            let currentThread = _.first(_.filter(this.props.threadSubmissions, ts => this.props.thread.id === ts.threadId));
+
+            if (!currentThread) {
+                this.cachedSubmissions = null;
+            } else {
+                this.cachedSubmissions = currentThread.submissions;
+            }
+        }
+        return this.cachedSubmissions ? _.filter(this.cachedSubmissions, this.themedFilter) : null;
     }
-    return this.cachedSubmissions ? _.filter(this.cachedSubmissions, this.themedFilter) : null;
-  }
 
-  fetchSubmissions() {
-    if (!this.submissions()) {
-      this.props.fetchSubmissions(this.props.thread.id, this.props.thread.url)
+    fetchSubmissions() {
+        if (!this.submissions()) {
+            this.props.fetchSubmissions(this.props.thread.id, this.props.thread.url);
+        }
     }
-  }
 
-  shouldShowGenerateScPlaylistButton() {
-    let submissions = this.submissions();
-    return submissions && submissions.length > 0
-      && _.filter(submissions, s => {
-        return _.filter(extractUrls(s.comment), u => isSoundCloudUrl(u)).length > 0;
-      }).length > 0;
-  }
+    shouldShowGenerateScPlaylistButton() {
+        let submissions = this.submissions();
+        return submissions && submissions.length > 0
+            && _.filter(submissions, s => {
+                return _.filter(extractUrls(s.comment), u => isSoundCloudUrl(u)).length > 0;
+            }).length > 0;
+    }
 
-  generateSoundcloudPlaylist() {
-    this.setState({ ...this.state, loadingPlaylist: true });
-    const title = this.props.thread.title;
-    generatePlaylist(_.flatten(_.map(this.submissions(), s => {
-        return _.filter(extractUrls(s.comment), u => isSoundCloudUrl(u))
-      })),
-      title,
-      this.props.isPrivateFilter)
-      .then(async () => {
-        // Slight delay as otherwise the playlist doesn't show up in Soundcloud API response
-        setTimeout(async () => this.setState({
-          ...this.state,
-          shouldShowPlaylist: true,
-          playlistTitle: title,
-          playlistLink: await getPlaylistLinkForThread(title),
-          loadingPlaylist: false,
-        }), 100);
-      });
-  }
+    generateSoundcloudPlaylist() {
+        this.setState({ loadingPlaylist: true });
+        const title = this.props.thread.title;
+        generatePlaylist(_.flatten(_.map(this.submissions(), s => {
+                return _.filter(extractUrls(s.comment), u => isSoundCloudUrl(u));
+            })),
+            title,
+            this.props.isPrivateFilter)
+            .then(async () => {
+                // Slight delay as otherwise the playlist doesn't show up in Soundcloud API response
+                setTimeout(async () => this.setState({
+                    ...this.state,
+                    shouldShowPlaylist: true,
+                    playlistTitle: title,
+                    playlistLink: await getPlaylistLinkForThread(title),
+                    loadingPlaylist: false,
+                }), 100);
+            });
+    }
 
-  shouldShowPlaylistLink() {
-    return this.state.shouldShowPlaylist && this.state.playlistLink;
-  }
+    shouldShowPlaylistLink() {
+        return this.state.shouldShowPlaylist && this.state.playlistLink;
+    }
 
-  shouldShowLoadingText() {
-    return this.state.loadingPlaylist;
-  }
+    shouldShowLoadingText() {
+        return this.state.loadingPlaylist;
+    }
 
-  render() {
-    const thread = this.props.thread;
-    const generateScPlaylistButton = (
-      <RaisedButton
-        style={{ marginBottom: '16px' }}
-        label='Generate Soundcloud Playlist'
-        onClick={this.generateSoundcloudPlaylist}/>
-    );
+    render() {
+        const thread = this.props.thread;
+        const generateScPlaylistButton = (
+            <RaisedButton
+                style={{ marginBottom: '16px' }}
+                label='Generate Soundcloud Playlist'
+                onClick={this.generateSoundcloudPlaylist}/>
+        );
 
-    const scPlaylistLink = (
-      <RaisedButton
-        style={{ marginBottom: '16px', marginLeft: "16px" }}
-        secondary
-        label="Soundcloud Playlist"
-        onClick={() => window.open(this.state.playlistLink)}/>
-    );
+        const scPlaylistLink = (
+            <RaisedButton
+                style={{ marginBottom: '16px', marginLeft: "16px" }}
+                secondary
+                label="Soundcloud Playlist"
+                onClick={() => window.open(this.state.playlistLink)}/>
+        );
 
-    return (
-      <Card
-        style={this.cardStyle}
-        onExpandChange={this.fetchSubmissions}
-      >
-        <CardHeader
-          title={theme(thread)}
-          subtitle={week(thread)}
-          actAsExpander
-          showExpandableButton
-        />
-        <Divider/>
-        <CardText expandable>
-          <ReactMarkdown source={description(thread)}/>
-
-          <Flexbox flexWrap='wrap' flexDirection='row' justifyContent='center'>
-            {this.shouldShowGenerateScPlaylistButton() ? generateScPlaylistButton : undefined}
-            {this.shouldShowLoadingText() ? <p>Generating playlist, please wait...</p> : undefined}
-            {this.shouldShowPlaylistLink() ? scPlaylistLink : undefined}
-          </Flexbox>
-
-          <Divider/>
-
-          <Flexbox style={this.flexboxStyle}
-                   flexDirection="column"
-                   flexWrap="wrap"
-                   flexBasis='content'
-          >
-            {_.map(this.submissions(), (s, i) => {
-              return (
-                <Submission
-                  key={s.author + i}
-                  author={s.author}
-                  comment={s.comment}
-                  url={`https://reddit.com/${s.subredditNamePrefixed}/comments/${s.threadId.replace('t3_', '')}/noop/${s.commentId.replace('t1_', '')}`}
+        return (
+            <Card
+                style={this.cardStyle}
+                onExpandChange={this.fetchSubmissions}
+            >
+                <CardHeader
+                    title={theme(thread)}
+                    subtitle={week(thread)}
+                    actAsExpander
+                    showExpandableButton
                 />
-              )
-            })}
-          </Flexbox>
+                <Divider/>
+                <CardText expandable>
+                    <ReactMarkdown source={description(thread)}/>
 
-        </CardText>
-      </Card>
-    );
-  };
+                    <Flexbox flexWrap='wrap' flexDirection='row' justifyContent='center'>
+                        {this.shouldShowGenerateScPlaylistButton() ? generateScPlaylistButton : undefined}
+                        {this.shouldShowLoadingText() ? <p>Generating playlist, please wait...</p> : undefined}
+                        {this.shouldShowPlaylistLink() ? scPlaylistLink : undefined}
+                    </Flexbox>
+
+                    <Divider/>
+
+                    <Flexbox style={this.flexboxStyle}
+                             flexDirection="column"
+                             flexWrap="wrap"
+                             flexBasis='content'
+                    >
+                        {_.map(this.submissions(), (s, i) => {
+                            return (
+                                <Submission
+                                    key={s.author + i}
+                                    author={s.author}
+                                    comment={s.comment}
+                                    url={`https://reddit.com/${s.subredditNamePrefixed}/comments/${s.threadId.replace('t3_', '')}/noop/${s.commentId.replace('t1_', '')}`}
+                                />
+                            );
+                        })}
+                    </Flexbox>
+
+                </CardText>
+            </Card>
+        );
+    };
 }
 
 const mapStateToProps = state => {
-  return {
-    threadSubmissions: _.filter(state.submissionThread.threadSubmissions),
-    isThemedFilter: state.filters.themed,
-    isPrivateFilter: state.filters.private,
-  }
+    return {
+        threadSubmissions: _.filter(state.submissionThread.threadSubmissions),
+        isThemedFilter: state.filters.themed,
+        isPrivateFilter: state.filters.private,
+    };
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    fetchSubmissions: (id, url) => {
-      dispatch(fetchSubmissions(id, url));
-    },
-  };
+    return {
+        fetchSubmissions: (id, url) => {
+            dispatch(fetchSubmissions(id, url));
+        },
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SubmissionThread);
---
+++ src/scenes/submissionThreads/components/SubmissionThread/SubmissionThread.test.js
@@ -0,0 +1,70 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { SubmissionThread } from './SubmissionThread';
+
+const getSubmissionThread = props => {
+    return shallow(
+        <SubmissionThread
+            thread={{}}
+            threadSubmissions={[]}
+            isPrivateFilter={false}
+            isThemedFilter={false}
+            {...props}
+        />
+    );
+};
+
+// TODO: Remove MUI so this selector can match on 'Submission'
+const getSubmissionComponents = wrapper => wrapper.find('MuiComponent');
+
+describe('SubmissionThread', () => {
+    it('renders a submission card per submission', () => {
+        const threadId = 'thread-id';
+        const thread = { id: threadId };
+        const submissions = [
+            { author: 'me', comment: 'comment', commentId: 'cId', threadId: 'tId'  },
+            { author: 'me', comment: 'comment2', commentId: 'cId2', threadId: 'tId'  },
+        ];
+        const threadSubmissions = [{ threadId: threadId, submissions }];
+        const wrapper = getSubmissionThread({ thread, threadSubmissions });
+        expect(getSubmissionComponents(wrapper).length).toBe(submissions.length);
+    });
+
+    describe('submission url', () => {
+        const threadId = 'thread-id';
+        const thread = { id: threadId };
+
+        const getSubmissionUrlForSubmission = submission => {
+            const threadSubmissions = [{
+                threadId: threadId,
+                submissions: [submission],
+            }];
+            const wrapper = getSubmissionThread({ thread, threadSubmissions });
+            return getSubmissionComponents(wrapper).first().prop('url');
+        };
+
+        it('matches expected format', () => {
+            const subredditNamePrefixed = 'sub';
+            const threadId = 'tId';
+            const commentId = 'cId';
+            const expectedUrl = `https://reddit.com/${subredditNamePrefixed}/comments/${threadId}/noop/${commentId}`;
+            const submission = { subredditNamePrefixed, threadId, commentId, comment: '' };
+
+            expect(getSubmissionUrlForSubmission(submission)).toEqual(expectedUrl);
+        });
+
+        it('trims t3_ prefix from thread Id', () => {
+            const threadId = 't3_tId';
+            const submission = { threadId, commentId: '', comment: '' };
+
+            expect(getSubmissionUrlForSubmission(submission)).not.toContain('t3_');
+        });
+
+        it('trims t1_ prefix from comment Id', () => {
+            const commentId = 't1_tId';
+            const submission = { threadId: '', commentId, comment: '' };
+
+            expect(getSubmissionUrlForSubmission(submission)).not.toContain('t1_');
+        });
+    });
+});
