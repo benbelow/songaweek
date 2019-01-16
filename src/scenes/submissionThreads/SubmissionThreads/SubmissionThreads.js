6596b35f76601b870440d3d97ba0485aeff21d2a
Ben Below <benjamin.below@softwire.com>
2019-01-16 13:35:22 +0000

    Only load some threads, add new thread button

--- src/scenes/submissionThreads/SubmissionThreads/SubmissionThreads.js
+++ src/scenes/submissionThreads/SubmissionThreads/SubmissionThreads.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { Button } from '@material-ui/core/es/index';
+
 import SubmissionThread from "../components/SubmissionThread/SubmissionThread";
 
 export class SubmissionThreads extends Component {
+    state = {
+        threadsToDisplay: 3,
+    };
+
     render() {
-        return (
+        const threadsLoaded = this.props.submissionThreads.length > 0;
+
+        return threadsLoaded ? (
             <div>
-                {this.props.submissionThreads.map(thread =>
+                {this.props.submissionThreads.slice(0, this.state.threadsToDisplay).map(thread =>
                     <SubmissionThread
                         key={thread.url}
                         thread={thread}
                     />
                 )}
+
+                <Button
+                    onClick={() => this.setState({ threadsToDisplay: this.state.threadsToDisplay + 10 })}
+                    style={{ marginBottom: 200, backgroundColor: '#AAAAAA' }}
+                >
+                    LOAD MORE THREADS
+                </Button>
             </div>
-        );
+        ) : <div>Loading threads...</div>;
     };
 }
 
